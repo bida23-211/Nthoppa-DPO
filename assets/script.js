1b63d601cb0fae90edd6cf32c381e0258cc3d2be
Junior Mosimanenkwe <166253208+bida23-211@users.noreply.github.com>
2026-02-11 11:14:36 +0200

    Update script.js
    Signed-off-by: Junior Mosimanenkwe <166253208+bida23-211@users.noreply.github.com>

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
+async function payNow() {
+  const response = await fetch("http://localhost:3000/api/payment/initiate", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      amount: 100,
+      currency: "BWP",
+      email: "test@example.com"
+    })
+  });
 
+  const data = await response.json();
+  window.location.href = data.paymentURL;
+}
