81d32856d6b219932aa3887c72c02615e31d7f88
Junior Mosimanenkwe <166253208+bida23-211@users.noreply.github.com>
2026-02-11 10:48:37 +0200

    server.js
    Signed-off-by: Junior Mosimanenkwe <166253208+bida23-211@users.noreply.github.com>

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 
+const paymentRoutes = require('./routes/payment');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/payment', paymentRoutes);
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
